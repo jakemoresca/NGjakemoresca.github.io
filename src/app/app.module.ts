f7350571de9327a4845a074338462ecb682bcca3
jakemoresca <jmoresca@outlook.com>
2021-07-16 16:00:40 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbModule],
-  declarations: [AppComponent, HelloComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    RouterModule.forRoot([{ path: '', component: AboutComponent }])
+  ],
+  declarations: [AppComponent, HelloComponent, AboutComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
